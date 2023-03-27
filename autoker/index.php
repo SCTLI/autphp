f90c54eb9d21e41a0610f10d4b2cb46cdfac5af8
SCTLI <galpatrik@tomorimajsa.hu>
2023-03-27 15:33:15 +0200

    alapcuccok nem sok de a mienk

---
+++ autoker/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Kezdo oldal</title>
+        <link rel="stylesheet" type="text/css" href="alap.css">
+        <link rel="icon" href="icon.png" type="image/icon">
+        <meta charset="utf-8" />
+    </head>
+    <body>
+                <nav>
+            <ul>
+                <li><a href="index.php">Kezdő Oldal</a></li>
+                <li><a href="autok.php">Autók tábla</a></li>
+                <li><a href="telephely.php">Telephelyek tábla</a></li>
+                <li><a href="elado.php">Eladók tábla</a></li>
+                <li><a href="muhely.php">Műhelyek tábla</a></li>
+                <li><a href="uzlet.php">Üzletek tábla</a></li>
+                <!--<li><a href="szerelo.php">Szerelők tábla</a></li> -->
+                <!--<li><a href="ugyfel.php">Ügyfelek tábla</a></li> -->
+                <!--<li><a href="vasarlasok.php">Vásárlások tábla</a></li> -->
+                <!--<li><a href="szerelesek.php">Szerelések tábla</a></li> -->
+            </ul>
+        </nav>
+        <h1>Üdvüzüljük az admin oldalon!</h1>
+        <h2>Az oldal még fejlesztés alatt van a kinézet megváltozhat, funkciók kerülhetnek beadásra!</h2>
+        <footer>
+            <p>&copy;Ami jog van az fent van tartva:)</p>
+        </footer>
+    </body>
+</html>
