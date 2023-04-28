7474894c4ebcc66dc8c97229f613c35cc6171ede
Recsit <foldimark20010915@gmail.com>
2023-04-28 12:10:26 +0200

    asd

--- autoker/bejelentkezes.php
+++ autoker/bejelentkezes.php
@@ -7,5 +7,6 @@
     <form action="login.php" method="post" accept-charset="utf-8">
         <input name="felh" type="text" placeholder="felh">
         <input name="jel" type="password" placeholder="jel">
+        <input type="submit" value="igen">
     </form>
 </body>
