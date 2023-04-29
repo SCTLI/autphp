d7d13efce14723a374bbcab46a142142a05b677b
Recsit <foldimark20010915@gmail.com>
2023-04-29 10:46:58 +0200

    funkciók

--- autoker/login.php
+++ autoker/login.php
@@ -3,11 +3,16 @@ session_start();
 $felhasz = $_POST['felh'];
 $jelsz = $_POST['jel'];
 
+if (FelhasznalonevVan($felhasz) ==1){
+    $_SESSION["felhasz"] = $_POST['felh'];
+    $_SESSION["jelsz"] = $_POST['jel'];
+    header("Location: index.php");
+}else{
+    header("Location: bejelentkezes.php");
 
-$_SESSION["felhasz"] = $_POST['felh'];
-$_SESSION["jelsz"] = $_POST['jel'];
+}
 
 
-header("Location: index.php");
+
 
 ?>
